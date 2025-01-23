1ffd44fe893743736876d1d0f3db66137a617ea1
Sebastian <sebastian.danielsson@proton.me>
2025-01-24 00:55:10 +0100

    Layout fix after tailwind v4

--- app/book/layout.tsx
+++ app/book/layout.tsx
@@ -24,11 +24,11 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
         <p className="text-muted-foreground">Boka tid i tvättstugan eller bastun.</p>
       </div>
       <Separator className="my-6" />
-      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="-mx-4 lg:w-1/5">
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-y-0 lg:gap-12">
+        <aside className="lg:w-1/5">
           <SidebarNav items={sidebarNavItems} />
         </aside>
-        <div className="flex-1 lg:max-w-2xl">{children}</div>
+        <div className="flex-1">{children}</div>
       </div>
     </div>
   );
